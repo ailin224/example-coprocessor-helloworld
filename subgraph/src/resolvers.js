98588780cb95eddaaab57c0f0e82cc9d81d20fa6
Aiko Lin <ailin@adobe.com>
2024-11-05 20:37:02 -0800

    Create images and videos

--- subgraph/src/resolvers.js
+++ subgraph/src/resolvers.js
@@ -1,7 +1,20 @@
+import { createImagesAndVideosWorkflowExecutor } from './workflowExecutor.js';
+
 const resolvers = {
   Query: {
     hello: () => {
       return "Hello World!";
+    },
+  },
+  Mutation: {
+    createImagesAndVideos: createImagesAndVideosWorkflowExecutor,
+  },
+  Asset: {
+    __resolveType(obj) {
+      if (obj.imageUrl) {
+        return 'Image';
+      }
+      return 'Video';
     },
   },
 };
